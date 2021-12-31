22aad2aaaeda8596e841d30ca82bc5d49e329b26
Seo Haechan <gocks241@gmail.com>
2021-12-31 22:57:43 +0900

    ongoing

--- BlackJackSimulation.c
+++ BlackJackSimulation.c
@@ -6,7 +6,7 @@
 /* initial condition */
 int N = 15;     // 전체 플레이어 수
 int p = 30;     // 카드를 뽑을 확률 [0 ~ 100(%)]
-int limit = 16; // 딜러가 게임을 정지할 조건
+int limit = 20; // 딜러가 게임을 정지할 조건
 int entire_deck[13] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11};      // 뽑을 수 있는 카드의 종류
 
 int rand_deck;  // 덱에서 어떤 카드를 뽑을지 결정하는 난수 (entire_deck과 연관있음)
@@ -72,7 +72,7 @@ void draw_card(int **player_arr, int *count_arr, int *sum_arr, int index)
             */
             count_arr[index] = 0;
             sum_arr[index] = 0;
-            printf("%dth player is dead\n", index);     // 디버그용 프린트
+            printf("%dth player is bursted\n", index);     // 디버그용 프린트
             return;
         }
         
@@ -86,7 +86,7 @@ void draw_card(int **player_arr, int *count_arr, int *sum_arr, int index)
     sum_arr[index] += drawn_card;
 }
 
-void dealer_draw(int *dealer_arr, int *count, int *sum)
+void dealer_draw(int *dealer_arr, int *count, int *sum, bool *judge)
 {
     rand_deck = rand()%13;                              // 어떤 카드를 뽑을지 (전역변수, 임시)
     drawn_card = entire_deck[rand_deck];                // 뽑은 카드 (전역변수, 임시)
@@ -113,6 +113,23 @@ void dealer_draw(int *dealer_arr, int *count, int *sum)
             drawn_card = 1;    // 그러면 손패가 1이 되어야지
             is_over_21 = false;
         }
+
+        if(is_over_21)
+        {
+            /* 21 넘겼으면 죽어야지 */
+
+            /* 덱 초기화. 근데 굳이 그럴 필요 있나 싶어서 덱은 냅두기
+            for (int j = 0; j < count_arr[index]; j++)
+            {
+                player_arr[index][j] = 0;
+            }
+            */
+            *count = 0;
+            *sum = 0;
+            printf("Dealer is bursted\n");     // 디버그용 프린트
+            *judge = true;
+            return;
+        }
         
     }
 
@@ -124,9 +141,10 @@ void dealer_draw(int *dealer_arr, int *count, int *sum)
     *sum += drawn_card;
 }
 
-void game_set(int *player_sum_arr, int *dealer_sum)
+void game_set()
 {
-    /* 게임 끝내기만 하면 되는데... 여기까지 오니까 게임 룰을 잘못 이해하고 있었던것 같음 */
+    /* 게임 끝내기 집계 */
+
 }
 
 int main()
@@ -196,6 +214,39 @@ int main()
         }
     }
 
+
+    for (int i = 0; i < N; i++)     // N명의 모든 사람들에게 확인
+    {
+        bool is_draw_done = false;  // draw가 끝났는지 확인하는 변수
+        while(!is_draw_done)
+        {
+            rand_draw = rand()%100;
+            if (rand_draw <= p)     // 카드를 뽑겠다 판단
+            {
+                draw_card(players, count_of_deck, sum_of_deck, i);
+            }
+            else        // 카드를 뽑지 않겠다 판단
+            {
+                is_draw_done = true;
+            }
+        }
+    }
+    
+
+    bool is_draw_done = false;
+    while(!is_draw_done)
+    {
+        if (dealer_sum < limit)     // 아직 Limit 안넘김
+        {   
+            dealer_draw(dealer, &dealer_count, &dealer_sum, &is_draw_done);
+        }
+        else        // Limit를 넘김
+        {
+            game_set();     // 게임 종료
+            is_draw_done = true;
+        }
+    }
+
     printf("======================= THIS IS FOR DEBUG =======================\n");
     printf("Dealer(%d) : [ ", dealer_count);
     for (int j = 0; j < dealer_count; j++)
@@ -215,40 +266,6 @@ int main()
     }
     printf("======================= THIS IS FOR DEBUG =======================\n");
 
-    bool is_bursted = false;
-    while(!is_bursted)
-    {
-        for (int i = 0; i < N; i++)
-        {
-            if (count_of_deck[i] != 0)  // 죽은사람 스킵
-            {
-                rand_draw = rand()%100;
-                if (rand_draw <= p)     // 뽑을지 말지 판단
-                {
-                   draw_card(players, count_of_deck, sum_of_deck, i);
-                }
-            }
-        }
-
-        if (dealer_sum >= limit)    // limit를 넘기면 게임을 중지시킴
-        {   
-            /* 종료 */
-            printf("=========================Dealer stop the game=========================");
-            //game_set();
-            is_bursted = true;
-        }
-        else
-        {
-            dealer_draw(dealer, &dealer_count, &dealer_sum);  // 딜러도 카드는 뽑아야지
-            if(dealer_sum > 21) // 뽑았더니 21이 넘었네
-            {
-                printf("=========================Dealer is bursted=========================");
-                //game_set();
-                is_bursted = true;
-            }
-        }
-    }
-    
     /* 동적할당 해제 */
     for (int i = 0; i < N; i++)
     {
